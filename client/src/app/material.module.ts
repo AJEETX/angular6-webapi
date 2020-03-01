ab832022b7235a07bbe2340f2e3a0afd7019680d
ajeet <ajeetx@email.com>
2020-03-01 17:07:03 +1100

    reorder display in list

---
+++ client/src/app/material.module.ts
@@ -0,0 +1,23 @@
+// import { NgModule  } from '@angular/core';
+// import { MatDatepickerModule} from '@angular/material/datepicker';
+//   import { MatFormFieldModule } from '@angular/material/form-field';
+//   import { MatInputModule } from '@angular/material/input';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// @NgModule({
+// imports: [
+// MatDatepickerModule,
+// MatFormFieldModule,
+// MatInputModule,
+// BrowserAnimationsModule
+// ],
+// exports: [
+// MatDatepickerModule,
+// MatFormFieldModule,
+// MatInputModule,
+// BrowserAnimationsModule
+// ],
+// providers: [ MatDatepickerModule ],
+// })
+
+// export class MaterialModule {}
